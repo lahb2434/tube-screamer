82d729022b005d142ad3f59c0e4b1f9b6e8d52fc
Christopher Houghton <ChrisHoughton@ChristopherMBP.fios-router.home>
2022-01-18 10:15:07 -0500

    remove unused code

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react'
+import { Container, Form, Stack, Button } from 'react-bootstrap';
+const SpotifyWebApi = require('spotify-web-api-node');
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.REACT_APP_CLIENT_ID,
+})
 
 export default class Dashboard extends Component {
 
   render() {
     const { accessToken } = this.props;
     return (
-      <div>
-      {accessToken}
-      </div>
+      <Container className="my-3">
+        <Stack direction="horizontal" gap={3}>
+          <Form.Control className="me-auto" placeholder="Search Artists/Songs" />
+          <Button variant="secondary">Submit</Button>
+        </Stack>
+      </Container>
     )
   }
 }
