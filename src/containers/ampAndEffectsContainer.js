bc42e841da2e646b833b2f7aa7db6a5e8e420d41
Christopher Houghton <ChrisHoughton@ChristopherMBP.fios-router.home>
2022-01-23 17:29:27 -0500

    build amplifier in react from one I built in js

--- src/containers/ampAndEffectsContainer.js
+++ src/containers/ampAndEffectsContainer.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import AmpAndEffects from '../components/ampAndEffects'
 
 export default function AmpAndEffectsContainer() {
   return <div>
-    
+    <AmpAndEffects />
   </div>;
 }
