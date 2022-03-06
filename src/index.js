2246d62986cf03f55882eef7715fd1ffb532790e
Christopher Houghton <ChrisHoughton@ChristopherMBP.fios-router.home>
2022-03-06 10:43:52 -0500

    add redux-persist to App.js for saving access token to store

--- src/index.js
+++ src/index.js
@@ -1,26 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import { Provider } from 'react-redux';
 import { createStore } from 'redux'
+
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import { PersistGate } from 'redux-persist/integration/react'
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import 'bootstrap/dist/css/bootstrap.min.css'
+
 import App from './App';
 import manageSearch from './reducers/manageSpotify';
 import Dashboard from './components/dashboard'
 
-const store = createStore(manageSearch, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, manageSearch)
+const store = createStore(persistedReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+const persistor = persistStore(store)
 
 ReactDOM.render(
     <Provider store={store}>
-      <BrowserRouter> 
-       <Routes>
-         <Route exact path="/" element={<App />} />
-         <Route path="/dashboard" element={<Dashboard />} />
-       </Routes>      
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter> 
+         <Routes>
+           <Route exact path="/" element={<App />} />
+           <Route path="/dashboard" element={<Dashboard />} />
+         </Routes>      
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   ,
   document.getElementById('root')
 );
-
-
