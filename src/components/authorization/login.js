6ef28324483b8758959f61214be3660be5503619
Chris Houghton <chrishoughton2434@gmail.com>
2022-05-31 15:27:03 -0400

    add spotify login button on dashboard

--- src/components/authorization/login.js
+++ src/components/authorization/login.js
@@ -4,12 +4,12 @@ import {Container} from 'react-bootstrap'
 
 export default function Login({AUTH_URL}) {
   return (
-    <div className="p-3 bg-dark bg-gradient">
-      <Container className="d-flex justify-content-center align-items-center" style={{ minHeight: "100vh" }}>
-        <a className="btn btn-dark btn-large" href={AUTH_URL} role="button">
-          Login With Spotify and save
+    // <div className="p-3 bg-dark bg-gradient">
+      /* <Container className="d-flex justify-content-center align-items-center" style={{ minHeight: "100vh" }}> */
+        <a className="fixed-bottom btn btn-dark btn-large" href={AUTH_URL} role="button">
+          Spotify
         </a>
-      </Container>
-    </div>
+      // </Container>
+    // </div>
   )
 }
